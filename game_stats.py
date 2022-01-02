a728b35376882ce223bc7f6a6790af8e31466cf5
Toby-Shaw <toby@jennifershaw.com>
2022-01-01 21:48:35 -0500

    First commit of finished book part

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+class Gamestats:
+    """Track stats for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # High score never resets.
+        f = open("/home/toby/Pythonthings/Alien_Invasion/high_score.txt", "r")
+        self.high_score = int(f.read())
+
+    def reset_stats(self):
+        """Initialize stats that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
